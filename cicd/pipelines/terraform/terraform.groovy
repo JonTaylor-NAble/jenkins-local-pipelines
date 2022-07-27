c56891a27fcb774853de5cd33cd6dcd5bbeeaa17
Jonathan Taylor <jonathan.taylor@n-able.com>
2022-07-27 14:05:04 +0100

    TF Pipeline

--- cicd/pipelines/terraform/terraform.groovy
+++ cicd/pipelines/terraform/terraform.groovy
@@ -1,3 +1,6 @@
+libraries {
+  lib('jenkins-local-shared-lib')
+}
 pipeline {
   agent any
   tools {
@@ -29,7 +32,7 @@ pipeline {
           
           sh 'terraform -chdir=./cicd/pipelines/terraform/ plan -no-color -out tf.plan'
 
-          def requiresWarning = checkForJenkinsMasterUpdates('tf.plan');
+          def requiresWarning = checkForJenkinsMasterUpdates 'tf.plan';
 
            if(requiresWarning){
             timeout(time: 5, unit: "MINUTES") {
@@ -40,7 +43,7 @@ pipeline {
                 input message: 'Do you want the deploy to Proceed?', ok: 'Yes'
             }
            }
-           
+
         }
       }
     }
@@ -63,58 +66,58 @@ pipeline {
   }
 }
 
-def checkForJenkinsMasterUpdates(planPath){
+// def checkForJenkinsMasterUpdates(planPath){
 
-  //tee(file: "tfPlan.log"){
-  sh 'terraform -chdir=./cicd/pipelines/terraform/ show -json ' + planPath + ' > tfPlan.log'
-  //}
+//   //tee(file: "tfPlan.log"){
+//   sh 'terraform -chdir=./cicd/pipelines/terraform/ show -json ' + planPath + ' > tfPlan.log'
+//   //}
 
-  def resultString = readFile(file: 'tfPlan.log');
-  def results = resultString.split('\n')
+//   def resultString = readFile(file: 'tfPlan.log');
+//   def results = resultString.split('\n')
 
-  def enhancedWarning = false;
-  def triggeringChange;
-  def output;
+//   def enhancedWarning = false;
+//   def triggeringChange;
+//   def output;
 
-  for (def result in results){
-    try{
-      output = readJSON text:result;
-    } catch (Exception ex){
-    }
-  }
+//   for (def result in results){
+//     try{
+//       output = readJSON text:result;
+//     } catch (Exception ex){
+//     }
+//   }
 
-  if (output){
-    if (output.resource_changes){
-      for (def resource_change in output.resource_changes){
-        if (resource_change.change){
-          if (resource_change.change.actions.indexOf('update') > -1){
+//   if (output){
+//     if (output.resource_changes){
+//       for (def resource_change in output.resource_changes){
+//         if (resource_change.change){
+//           if (resource_change.change.actions.indexOf('update') > -1){
 
-            def manifestBeforeStr = resource_change.change.before.manifest;
-            def manifestAfterStr = resource_change.change.after.manifest;
+//             def manifestBeforeStr = resource_change.change.before.manifest;
+//             def manifestAfterStr = resource_change.change.after.manifest;
 
-            def manifestBefore = readJSON text:manifestBeforeStr;
-            def manifestAfter = readJSON text:manifestAfterStr;
+//             def manifestBefore = readJSON text:manifestBeforeStr;
+//             def manifestAfter = readJSON text:manifestAfterStr;
 
-            if (manifestBefore.kind == 'Jenkins'){
-              if (manifestBefore.spec && manifestBefore.spec.master){
-                def compBefore = manifestBefore.spec.master;
-                def compAfter = manifestAfter.spec.master;
+//             if (manifestBefore.kind == 'Jenkins'){
+//               if (manifestBefore.spec && manifestBefore.spec.master){
+//                 def compBefore = manifestBefore.spec.master;
+//                 def compAfter = manifestAfter.spec.master;
 
-                if (compBefore != compAfter){
-                  enhancedWarning = true;
-                  triggeringChange = compAfter;
-                  echo "Master node config is changed"
-                } 
-              }
-            } 
-          }
-        }
-      }
-    }
-  } else {
-    echo "Invalid TF Plan JSON"
-  }
+//                 if (compBefore != compAfter){
+//                   enhancedWarning = true;
+//                   triggeringChange = compAfter;
+//                   echo "Master node config is changed"
+//                 } 
+//               }
+//             } 
+//           }
+//         }
+//       }
+//     }
+//   } else {
+//     echo "Invalid TF Plan JSON"
+//   }
 
-  sh 'rm -f tfPlan.log'
-  return enhancedWarning;
-}
+//   sh 'rm -f tfPlan.log'
+//   return enhancedWarning;
+// }
