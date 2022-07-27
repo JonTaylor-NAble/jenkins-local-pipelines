6e31d46d0cc586906463299ec647e91daeebf4bc
Jonathan Taylor <jonathan.taylor@n-able.com>
2022-07-27 11:23:58 +0100

    TF Pipeline

--- cicd/pipelines/terraform/terraform.groovy
+++ cicd/pipelines/terraform/terraform.groovy
@@ -58,6 +58,8 @@ pipeline {
   }
   post {
     always {
+      sh 'gitconfig --global user.email "jonathan.taylor@n-able.com"'
+      sh 'git config --global user.name "Jon Taylor"'
       sh 'git add .'
       sh 'git commit -m "Post build commit"'
       sh 'git push'
