b28bd39b33dfc8408ae7e42b9f8d8c4f4c352f1f
Jonathan Taylor <jonathan.taylor@n-able.com>
2022-08-09 15:08:19 +0100

    Utilize seed job factory

--- seed/jobs/buildSeed.groovy
+++ seed/jobs/buildSeed.groovy
@@ -1,4 +1,15 @@
-@Library('northstar')
-import northstar
+def gitCredentialsId      = 'github-account'
+def jobsRepoName          = 'https://github.com/jontaylor-nable/jenkins-local-pipelines.git'
+def sharedLibraryRepoName = 'https://github.com/jontaylor-nable/jenkins-local-shared-lib.git'
 
-northstar.buildSeedJobs(null)
+
+checkout([
+    $class: 'GitSCM', 
+    branches: [[name: '*/main']], 
+    doGenerateSubmoduleConfigurations: false,
+    extensions: [[$class: 'RelativeTargetDirectory', relativeTargetDir: 'shared-library']], 
+    submoduleCfg: [],
+    userRemoteConfigs: [[credentialsId:  gitCredentialsId, url: sharedLibraryRepoName ]]
+    ])
+
+git url: jobsRepoName, changelog: false, credentialsId: gitCredentialsId, poll: false, branch: 'main'
