2e0f5d5d6d8e194f7695f370f316d7ade37b077f
Jonathan Taylor <jonathan.taylor@n-able.com>
2022-08-09 13:41:23 +0100

    Utilize seed job factory

--- seed/jobs/buildSeed.groovy
+++ seed/jobs/buildSeed.groovy
@@ -2,25 +2,30 @@ def gitCredentialsId      = 'github-account'
 def jobsRepoName          = 'https://github.com/jontaylor-nable/jenkins-local-pipelines.git'
 def sharedLibraryRepoName = 'https://github.com/jontaylor-nable/jenkins-local-shared-lib.git'
 
-pipeline {
-    agent any
-    stages{
-        stage('Seed Job') {
+pipelineJob {
+    definition{
+        pipeline{
             agent any
-            steps {
+            stages{
+                stage('Seed Job') {
+                    agent any
+                    steps {
 
-                checkout([
-                    $class: 'GitSCM', 
-                    branches: [[name: '*/main']], 
-                    doGenerateSubmoduleConfigurations: false,
-                    extensions: [[$class: 'RelativeTargetDirectory', relativeTargetDir: 'shared-library']], 
-                    submoduleCfg: [],
-                    userRemoteConfigs: [[credentialsId:  gitCredentialsId, url: sharedLibraryRepoName ]]
-                    ])
+                        checkout([
+                            $class: 'GitSCM', 
+                            branches: [[name: '*/main']], 
+                            doGenerateSubmoduleConfigurations: false,
+                            extensions: [[$class: 'RelativeTargetDirectory', relativeTargetDir: 'shared-library']], 
+                            submoduleCfg: [],
+                            userRemoteConfigs: [[credentialsId:  gitCredentialsId, url: sharedLibraryRepoName ]]
+                            ])
 
-                git url: jobsRepoName, changelog: false, credentialsId: gitCredentialsId, poll: false, branch: 'main'
-                jobDsl targets: 'seed/jobs/**/pipeline.groovy', additionalClasspath: 'shared-library/src'
+                        git url: jobsRepoName, changelog: false, credentialsId: gitCredentialsId, poll: false, branch: 'main'
+                        jobDsl targets: 'seed/jobs/**/pipeline.groovy', additionalClasspath: 'shared-library/src'
+                    }
+                }
             }
         }
     }
+    
 }
