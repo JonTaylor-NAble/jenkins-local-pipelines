8c3d9f94e4b2280cda464abf267e9119b0b6600b
Jonathan Taylor <jonathan.taylor@n-able.com>
2022-08-09 14:04:50 +0100

    Utilize seed job factory

--- seed/jobs/buildSeed.groovy
+++ seed/jobs/buildSeed.groovy
@@ -1,19 +1,3 @@
-#!/usr/bin/env groovy
+library 'jenkins-local-shared-lib'
 
-pipelineJob('build-operator-service-for-jenkins') {
-    displayName('Build Operator Service for Jenkins')
-
-    definition {
-        cpsScm {
-            scm {
-                git {
-                    remote {
-                        url('https://github.com/jontaylor-nable/jenkins-local-pipelines.git')
-                    }
-                    branches('*/main')
-                }
-            }
-            scriptPath('cicd/pipelines/example/exampleBuild.groovy')
-        }
-    }
-}
+northstar.buildSeedJobs(null)
